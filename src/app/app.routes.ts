7109d6f30f9f9b30f348750140fe45b977541e26
Kyra <kyra.dev.07@gmail.com>
2025-05-24 15:01:49 -0500

    se crean los componentes a usar, se organizan las rutas de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,34 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'products',
     component: ProductsListComponent
+  },
+  {
+    path: 'products/add',
+    component: ProductFormComponent
+  },
+  {
+    path: 'categories',
+    component: CategoriesListComponent
+  },
+  {
+    path: 'categories/add',
+    component: CategoryFormComponent
+  },
+  {
+    path: 'users',
+    component: UsersListComponent
+  },
+  {
+    path: 'users/add',
+    component: UserFormComponent
   }
 ];
