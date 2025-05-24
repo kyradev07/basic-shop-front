7e195eee245b8e767296c272a5e0c0a20c7b28cf
Kyra <kyra.dev.07@gmail.com>
2025-05-24 14:23:06 -0500

    se agrega componente navbar, footer, lista productos, estilos y el cdn a los íconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'shop-admin';
+
+  isOpenedMenu: boolean = false;
+
+  openMenu(): void {
+    this.isOpenedMenu = true;
+  }
+
+  closeMenu(): void {
+    this.isOpenedMenu = false;
+  }
 }
