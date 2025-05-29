9fd5cad300ecc669839fdd7884b2d71b1b67ca6c
Kyra <kyra.dev.07@gmail.com>
2025-05-29 00:03:10 -0500

    se crea servicio para el manejo para las operaciones crud relaciones a productos

---
+++ src/app/services/product/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/product.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private readonly url = 'http://localhost:3000/product';
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.url, product);
+  }
+
+}
