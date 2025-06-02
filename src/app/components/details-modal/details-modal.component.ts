6f0c89047dad218855018ed0506d513a2d586438
Kyra <kyra.dev.07@gmail.com>
2025-06-02 01:49:47 -0500

    se crea componente para mostrar detalle de información en modal

---
+++ src/app/components/details-modal/details-modal.component.ts
@@ -0,0 +1,34 @@
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Product } from '../../models/product.interface';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-details-modal',
+  imports: [
+    CarouselComponent,
+    CurrencyPipe,
+    UpperCasePipe
+  ],
+  templateUrl: './details-modal.component.html',
+  styleUrl: './details-modal.component.css'
+})
+export class DetailsModalComponent {
+
+  @ViewChild('myModal') detailModal!: ElementRef<HTMLDialogElement>
+  @Input() product!: Product;
+
+  cancel() {
+    this.detailModal.nativeElement.close();
+  }
+
+  getCategory(product: Product) {
+    return typeof product.category === 'number' ? product.category : product.category.name;
+  }
+
+  onKeyDown($event: KeyboardEvent) {
+    if ($event.key === 'Escape') {
+      $event.preventDefault();
+    }
+  }
+}
