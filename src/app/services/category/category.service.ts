0c2968075a533ede4e93c49ebeca2de5f9ce2572
Kyra <kyra.dev.07@gmail.com>
2025-06-10 14:39:06 -0500

    se modifica importación del archivo environment

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../../models/category.interface';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
